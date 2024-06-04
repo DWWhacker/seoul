29e0dfa43ba1d8f5711f44b6622ef6fe467c3454
chajoonho <167054625+chajoonho@users.noreply.github.com>
2024-06-04 16:51:58 +0900

    ,
    ,

---
+++ 20240530/firebase.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>파이어베이스 연습</title>
+  </head>
+  <body></body>
+  <script type="module">
+    import { db, getMembers } from '../resources/js/firebase.js';
+    getMembers();
+  </script>
+</html>
+asybc function get getMembers(){
+    const collect 
+    const snapshat
+
+}
+console.log(snapshot);
+snapshot.forEach(doc.data)
+
+// 데이터 함수를 사용해서 for Each_반복문
+ 
